2f0c894bd797d477c22927fec2b8af18d1b58708
DESKTOP-0UPI14J\hp <mmbalisi@csmbataan.ph.education>
2020-10-27 00:03:28 +0800

    Creating a file, Finishing the Layout Window and Add Working Button.

---
+++ MarkBalisi.FIRST QUARTERLY ASSESSMENT/MarkBalisi.FIRST QUARTERLY ASSESSMENT/Personal Info/PersonalInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MarkBalisi.FIRST_QUARTERLY_ASSESSMENT.Personal_Info
+{
+    public class PersonalInfo
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string born { get; set; }
+    }
+}
---
+++ MarkBalisi.FIRST QUARTERLY ASSESSMENT/MarkBalisi.FIRST QUARTERLY ASSESSMENT/Personal Info/PersonalInfoWindows.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarkBalisi.FIRST_QUARTERLY_ASSESSMENT.Personal_Info
+{
+    /// <summary>
+    /// Interaction logic for PersonalInfoWindows.xaml
+    /// </summary>
+    public partial class PersonalInfoWindows : Window
+    {
+        public PersonalInfoWindows()
+        {
+            InitializeComponent();
+            PersonalInfo personalInfo = new PersonalInfo();
+            using (StreamReader r = new StreamReader("Data\\json6.json"))
+            {
+                string json = r.ReadToEnd();
+                personalInfo = JsonConvert.DeserializeObject<PersonalInfo>(json);
+            }
+            lblPeronalInfoId.Content = "Id: " + personalInfo.id;
+            lblPeronalInfoName.Content = "Name: " + personalInfo.name;
+            lblPeronalInfoBorn.Content = "Born: " + personalInfo.born;
+        }
+    }
+}
